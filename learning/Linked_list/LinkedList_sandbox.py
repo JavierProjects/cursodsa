7387f8e936c313b84f8284acbd521371563aa0d7
Javier <cuadros.jav@gmail.com>
2024-10-02 10:17:48 -0600

    Se agregaron comentarios de ayuda a: - learning/Linked_list/LinkedList_sandbox.py - learning/Linked_list/Node.py

--- learning/Linked_list/LinkedList_sandbox.py
+++ learning/Linked_list/LinkedList_sandbox.py
@@ -1,1 +1,2 @@
-# Escribe debajo el codigo de la clase LinkedList y sus respectivos metodos        
+# Escribe debajo el codigo de la clase LinkedList y sus respectivos metodos     
+# Recuerda importar la clase Node en este script
