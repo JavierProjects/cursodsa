3d99af549fa23abbed18c9317a0e321952d8aa1e
Javier <cuadros.jav@gmail.com>
2024-12-06 09:32:25 -0600

    Se agrego el juego de las torres de Hanoi

---
+++ learning/Stacks/HanoiGame.py
@@ -0,0 +1,17 @@
+from StackHanoi import Stack
+
+print("\n¡Vamos a jugar a las torres de Hanoi!")
+
+#Crear las pilas
+
+
+
+#Configurar el juego
+
+
+
+#Obtener entrada del usuario
+
+
+        
+#Jugando el juego
---
+++ learning/Stacks/StackHanoi.py
@@ -0,0 +1,53 @@
+from Node import Node
+import logging
+
+
+class Stack:
+  def __init__(self, name):
+    self.size = 0
+    self.top_item = None
+    self.limit = 1000
+    self.name = name
+  
+  def push(self, value):
+    if self.has_space():
+      item = Node(value)
+      item.set_next_node(self.top_item)
+      self.top_item = item
+      self.size += 1
+    else:
+      logging.warning('La pila esta llena ¡No queda espacio!')
+
+  def pop(self):
+    if self.size > 0:
+      item_to_remove = self.top_item
+      self.top_item = item_to_remove.get_next_node()
+      self.size -= 1
+      return item_to_remove.get_value()
+    logging.warning('La pila esta totalmente vacia!')
+
+  def peek(self):
+    if self.size > 0:
+      return self.top_item.get_value()
+    logging.warning('La pila esta totalmente vacia!')
+
+  def has_space(self):
+    return self.limit > self.size
+
+  def is_empty(self):
+    return self.size == 0
+  
+  def get_size(self):
+    return self.size
+  
+  def get_name(self):
+    return self.name
+  
+  def print_items(self):
+    pointer = self.top_item
+    print_list = []
+    while(pointer):
+      print_list.append(pointer.get_value())
+      pointer = pointer.get_next_node()
+    print_list.reverse()
+    print("{0} Stack: {1}".format(self.get_name(), print_list))
